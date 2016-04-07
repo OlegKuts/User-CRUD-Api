87c8b8e0061792895fbdf9b538b94508c329d214
Oleg Kuts <kuts.oleg91@gmail.com>
2016-04-07 22:35:56 +0300

    API controller added

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :api do
+    resources :users, defaults: { :format => 'json' }
+  end
 end
