17b9552a5d9a39d9a07342cb92c9e376bdfdac7f
Oleg Kuts <kuts.oleg91@gmail.com>
2016-04-08 18:15:19 +0300

    users table created

--- app/assets/javascripts/components/hello_world.js.jsx
+++
@@ -1,5 +0,0 @@
-var HelloWorld = React.createClass({
-	render:function(){
-		return <div className="btn btn-danger"><h1 >Hello World</h1></div>;
-	}
-});
--- app/assets/javascripts/components/users.js.jsx
+++ app/assets/javascripts/components/users.js.jsx
@@ -1,1 +1,54 @@
-users.js.jsx
+var Layout = React.createClass({
+	render:function(){
+		return 	<div className="container">
+					<div className="row"><UserBox/></div>
+				</div>;
+	}
+});
+
+var UserBox = React.createClass({
+
+	getInitialState: function() {
+	    return {users: []};
+	},
+
+	componentDidMount: function() {
+	    $.ajax({
+	      url: "http://localhost:3000/api/users",
+	      //dataType: 'json',
+	      cache: false,
+	      success: function(data) {
+	        this.setState({users: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.props.url, status, err.toString());
+	      }.bind(this)
+	    });
+	},
+	render:function(){
+		var users = this.state.users;
+		return <UserTable users = {users}/>;
+	}
+});
+
+var UserTable = React.createClass({
+
+	render: function(){
+		var users = this.props.users.map(function(user){
+			return <User user={user} key={user.id}/>
+		});
+		return <table className="table-bordered"><tbody>{users}</tbody></table>;
+	}
+})
+
+var User = React.createClass({
+	render:function(){
+		var { name, surname, country, age } = this.props.user;
+		return 	<tr>
+					<td>{name}</td>
+					<td>{surname}</td>
+					<td>{country}</td>
+					<td>{age}</td>
+				</tr>;
+	}
+})
